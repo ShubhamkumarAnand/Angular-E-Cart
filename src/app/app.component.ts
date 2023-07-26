5f1edeb9885e0bd1b316dce1dab3d20a1e968539
imskanand <the.anand3099@gmail.com>
2023-07-26 14:04:25 +0530

    Bootstrapping the Angular project to the only Hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-ekart';
+  title = 'E-Commerce Application made Using the Angular Framework';
 }
